3f22373b49f0043009716e22d107741d3e9398cb
deejay-oukay <dj-ok@mail.ru>
2023-01-30 18:36:12 +0500

    Домашние задания решены до просмотра семинара. Возможны правки

---
+++ Task64/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+
+namespace MyNameSpace
+{
+    class Task64
+    {
+        static private int InputInt(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static private void PrintNextValue(int value)
+        {
+            if (value == 1)
+                System.Console.WriteLine(value);
+            else
+            {
+                System.Console.Write(value + ", ");
+                if (value > 1)
+                    PrintNextValue(value - 1);
+                else
+                    PrintNextValue(value + 1);
+            }
+        }
+
+        static public void Main()
+        {
+            PrintNextValue(InputInt("Введите число N: "));
+        }
+    }
+}
+
---
+++ Task66/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+
+namespace MyNameSpace
+{
+    class Task66
+    {
+        static int m, n, sum;
+        static private int InputInt(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static private void Sum(int from, int to)
+        {
+            sum += from;
+            if (from < to)
+            {
+                Sum(from + 1, to);
+            }
+            else if (from > to)
+            {
+                Sum(from - 1, to);
+            }
+        }
+
+        static public void Main()
+        {
+            m = InputInt("Введите число M: ");
+            n = InputInt("Введите число N: ");
+            Sum(m, n);
+            System.Console.WriteLine($"Сумма чисел от {m} до {n}: {sum}");
+        }
+    }
+}
+
